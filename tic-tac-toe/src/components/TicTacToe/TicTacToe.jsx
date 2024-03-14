1c8b4b775d45434d41acf7742854472834b5ceb7
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-14 11:50:03 +0200

    updated

--- tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe/TicTacToe.jsx
@@ -8,9 +8,24 @@ const TicTacToe = () => {
     <div className='container'>
       <h1 className='title'>Tic Tac Toe</h1>
       <div className="board">
-
-      </div>
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        </div>
       <button className="reset">Reset</button>
+
     </div>
   );
 }
